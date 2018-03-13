561b0aa80df32e8a477744c3ebc23f4dfa4ba0f7
JamesThomasEdwards <digitalmediapro7@gmail.com>
2018-03-12 21:28:34 -0700

    completed mongodb overriding methods

--- libraries/mongo.js
+++ libraries/mongo.js
@@ -4,7 +4,7 @@ const mongo = require("mongodb-core");
 let original = mongo.Server.prototype.insert;
 let library = "MongoDB";
 
-mongo.Server.prototype.insert = function() {
+mongo.Server.prototype.insert = function () {
   if (arguments.length > 0) {
     let trace = NRA.createTrace(library, "Insert");
 
@@ -12,7 +12,7 @@ mongo.Server.prototype.insert = function() {
     const cb = arguments[index];
 
     if (typeof cb === "function") {
-      arguments[index] = function() {
+      arguments[index] = function () {
         trace.end();
         return cb.apply(this, arguments);
       };
@@ -24,7 +24,7 @@ mongo.Server.prototype.insert = function() {
 
 let findOriginal = mongo.Cursor.prototype._find;
 
-mongo.Cursor.prototype._find = function() {
+mongo.Cursor.prototype._find = function () {
   console.log('nra agent mongo');
   if (arguments.length > 0) {
     let trace = NRA.createTrace(library, "Find");
@@ -32,7 +32,7 @@ mongo.Cursor.prototype._find = function() {
     const orig_cb = arguments[0];
 
     if (typeof orig_cb === "function") {
-      arguments[0] = function() {
+      arguments[0] = function () {
         trace.end();
         console.log("trace ended:  " + trace.traceTimer.startTimestamp);
         return orig_cb.apply(this, arguments);
@@ -43,3 +43,77 @@ mongo.Cursor.prototype._find = function() {
   return findOriginal.apply(this, arguments);
 };
 
+let updateOriginal = mongo.Server.prototype.update
+
+mongo.Server.prototype.update = function () {
+  if (arguments.length > 0) {
+    let trace = NRA.createTrace(library, "Update");
+
+    const index = arguments.length - 1
+    const cb = arguments[index]
+    if (typeof cb === "function") {
+      arguments[index] = function () {
+        trace.end()
+        return cb.apply(this, arguments);
+      }
+    }
+  }
+  return updateOriginal.apply(this, arguments);
+}
+
+let removeOriginal = mongo.Server.prototype.remove
+
+mongo.Server.prototype.remove = function () {
+  if (arguments.length > 0) {
+    let trace = NRA.createTrace(library, "Remove");
+
+    const index = arguments.length - 1;
+    const cb = arguments[index];
+    if (typeof cb === "function") {
+      arguments[index] = function () {
+        trace.end()
+        return cb.apply(this, arguments);
+      }
+    }
+  }
+  return removeOriginal.apply(this, arguments);
+}
+
+
+let authOriginal = mongo.Server.prototype.auth
+
+mongo.Server.prototype.auth = function () {
+  if (arguments.length > 0) {
+    let trace = NRA.createTrace(library, "Auth");
+
+    const index = auguments.length - 1;
+    const cb = arguemtns[index];
+    if (typeof cb === "function") {
+      arguments[index] = function () {
+        trace.end()
+        return cb.apply(this, arguments);
+      }
+    }
+  }
+  return authOriginal.apply(this, arguments);
+}
+
+let getMoreOriginal = mongo.Cursor.prototype._getmore;
+
+mongo.Cursor.prototype._getmore = function () {
+  if (arguments.length > 0) {
+    let trace = NRA.createTrace(library, "GetMore");
+
+    const cb = arguments[0];
+
+    if (typeof cb === "function") {
+      arguments[0] = function () {
+        trace.end();
+        console.log("trace ended:  " + trace.traceTimer.startTimestamp);
+        return cb.apply(this, arguments);
+      };
+    }
+  }
+
+  return getMoreOriginal.apply(this, arguments);
+};
