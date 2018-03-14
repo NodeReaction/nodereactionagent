fa1c5bb7b9dba5295b057669598499777bfbb318
Eric Fileti <github.com@ericfileti.com>
2018-03-14 16:27:39 -0700

    update to imports and tweaked package json for npm

--- NodeReaction.js
+++ NodeReaction.js
@@ -1,5 +1,9 @@
 const Agent = require("./lib/Agent");
 const httpLib = require("./lib/modules/http");
 const mongoLib = require("./lib/modules/mongo");
+// const mysqlLib = require("./lib/modules/mySql");
+// const postreSqlLib = require("./lib/modules/postreSql");
+// const fsLib = require("./lib/modules/fs");
+// const reddisLib = require("./lib/modules/reddis");
 
 module.exports = Agent;
