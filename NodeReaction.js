d2af3a2282c32a6d3f4a21cc5de3710cd2f42b0c
Eric Fileti <github.com@ericfileti.com>
2018-03-22 16:20:56 -0700

    turned off auto load of mongo

--- NodeReaction.js
+++ NodeReaction.js
@@ -1,6 +1,6 @@
 const d = new Date(Date.now()).toISOString().slice(0, 23).replace("T", " ");
 console.log(`
-=========== Node Reaction Agent 0.2.6 ===========\n
+=========== Node Reaction Agent 0.2.7 ===========\n
 time data sent: ${d}
 `);
 const Agent = require("./lib/Agent");
