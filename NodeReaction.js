5114a2aeffe06c192d86de20c467b68d46909629
Eric Fileti <github.com@ericfileti.com>
2018-03-22 16:06:21 -0700

    updated agent to 2.6 removed console logs

--- NodeReaction.js
+++ NodeReaction.js
@@ -1,6 +1,6 @@
 const d = new Date(Date.now()).toISOString().slice(0, 23).replace("T", " ");
 console.log(`
-=========== Node Reaction Agent 0.2.5 ===========\n
+=========== Node Reaction Agent 0.2.6 ===========\n
 time data sent: ${d}
 `);
 const Agent = require("./lib/Agent");
