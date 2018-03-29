64e55bd1857d28db8204c5e181ea860fe9c4574d
Eric Fileti <github.com@ericfileti.com>
2018-03-28 23:11:44 -0700

    reworked logging, data export handling

--- NodeReaction.js
+++ NodeReaction.js
@@ -1,9 +1,4 @@
-const logger = require("./lib/logger");
-const d = new Date(Date.now()).toISOString().slice(0, 23).replace("T", " ");
-logger.log(`
-=========== Node Reaction Agent 0.2.9 ===========\n
-time data sent: ${d}
-`);
+const logger = require("./lib/logger").logStartup();
 const Agent = require("./lib/Agent");
 const libraries = require("./lib/LibraryLoader");
 module.exports = Agent;
